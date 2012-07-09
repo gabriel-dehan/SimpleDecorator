d33887489a86dbcfde3fa66a533412bc5e0544f0
gabriel-dehan <dehan.gabriel@gmail.com>
2012-07-09 23:00:09 +0200

    SimpleDecorators are now fully transparent

--- lib/simple_decorator.rb
+++ lib/simple_decorator.rb
@@ -1,20 +1,29 @@
-# SimpleDecorator allows you to give true decorator behavior to your classes.
-#
-# Author:  Gabriel Dehan (gabriel-dehan)
-#
-# License: WTFPL (http://sam.zoy.org/wtfpl/COPYING)
-class SimpleDecorator < BasicObject
-  undef_method :==
+require 'delegate'
 
+# Author::  Gabriel Dehan (gabriel-dehan)
+# License:: WTFPL (http://sam.zoy.org/wtfpl/COPYING)
+#
+# Allows you to give true decorator behavior to your classes.
+class SimpleDecorator < Delegator
+  # We must undefine those methods, for they will be sent to the underlying
+  # object
+  undef_method :==
+  undef_method :class
+  undef_method :instance_of?
+
+  # Stores the decorated object
   def initialize(component)
+    super
     @component = component
   end
 
+  # Returns the component
   def decorated
     @component
   end
   alias :source :decorated
 
-  def method_missing(n, *a, &b); @component.send(n, *a, &b) end
-  def send(s, *a); __send__(s, *a) end
+  def __setobj__(o); @component = o   end
+  def __getobj__;    @component       end
+  def send(s, *a);   __send__(s, *a)  end
 end
--- simple_decorator.gemspec
+++ simple_decorator.gemspec
@@ -1,10 +1,11 @@
 Gem::Specification.new do |s|
-  s.name     = 'simple_decorator'
-  s.version  = '0.1.0'
-  s.date     = '2012-07-09'
-  s.summary  = 'Implements true decorators in Ruby'
-  s.authors  = ['Gabriel Dehan']
-  s.email    = 'dehan.gabriel@gmail.com'
-  s.files    = ['lib/simple_decorator.rb']
-  s.homepage = 'http://rubygems.org/gems/simple_decorator'
+  s.name         = 'simple-decorator'
+  s.version      = '0.1.2'
+  s.date         = '2012-07-09'
+  s.summary      = 'True decorators in Ruby'
+  s.description  = 'Implements true decorators in Ruby'
+  s.authors      = ['GabrielDehan']
+  s.email        = 'dehan.gabriel@gmail.com'
+  s.files        = ['lib/simple_decorator.rb']
+  s.homepage     = 'http://rubygems.org/gems/simple-decorator'
 end
